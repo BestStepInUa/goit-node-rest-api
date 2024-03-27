07245162c07f98a2b0bc8af6c79b57c838122a6a
BestStepInUa <beststep.in.ua@gmail.com>
2024-03-27 19:46:09 +0200

    REXEP bug FIXED

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,15 +1,19 @@
 import Joi from 'joi';
-import { EMAIL_REXEP } from '../constants/contactsConstants.js';
+// import { PHONE_REXEP, EMAIL_REXEP } from '../constants/contactsConstants.js';
 
 export const addContactSchema = Joi.object({
   name: Joi.string().required(),
-  email: Joi.string().pattern(EMAIL_REXEP).required(),
+  // email: Joi.string().pattern(EMAIL_REXEP).required(),
+  // phone: Joi.string().pattern(PHONE_REXEP).required(),
+  email: Joi.string().required(),
   phone: Joi.string().required(),
   favorite: Joi.boolean().required(),
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string(),
+  // email: Joi.string().pattern(EMAIL_REXEP),
+  // phone: Joi.string().pattern(PHONE_REXEP),
   email: Joi.string(),
   phone: Joi.string(),
   favorite: Joi.boolean(),
