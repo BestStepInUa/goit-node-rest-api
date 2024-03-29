8e1454bb7e2b5ff83db5007727459a89cf729b6f
BestStepInUa <beststep.in.ua@gmail.com>
2024-03-29 23:42:28 +0200

    next in controllers del

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -2,38 +2,38 @@ import contactsServices from '../services/contactsServices.js';
 import ctrlWrapper from '../decorators/ctrlWrapper.js';
 import HttpError from '../helpers/HttpError.js';
 
-const getAll = async (req, res, next) => {
+const getAll = async (req, res) => {
   const result = await contactsServices.listContacts();
   res.json(result);
 };
 
-const getById = async (req, res, next) => {
+const getById = async (req, res) => {
   const { id } = req.params;
   const result = await contactsServices.getContactById(id);
   if (!result) throw HttpError(404, `Not found`);
   res.json(result);
 };
 
-const addContact = async (req, res, next) => {
+const addContact = async (req, res) => {
   const result = await contactsServices.addContact(req.body);
   res.status(201).json(result);
 };
 
-const updateContact = async (req, res, next) => {
+const updateContact = async (req, res) => {
   const { id } = req.params;
   const result = await contactsServices.updateContactById(id, req.body);
   if (!result) throw HttpError(404, `Not found`);
   res.json(result);
 };
 
-const deleteContact = async (req, res, next) => {
+const deleteContact = async (req, res) => {
   const { id } = req.params;
   const result = await contactsServices.removeContactById(id);
   if (!result) throw HttpError(404, `Not found`);
   res.json(result);
 };
 
-const updateContactStatus = async (req, res, next) => {
+const updateContactStatus = async (req, res) => {
   const { id } = req.params;
   const result = await contactsServices.updateContactStatusById(id, req.body);
   if (!result) throw HttpError(404, `Not found`);
