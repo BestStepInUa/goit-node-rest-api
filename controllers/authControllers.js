afda25c210b7dfc7c32b1dadeded0598002ca666
BestStepInUa <beststep.in.ua@gmail.com>
2024-04-04 18:25:31 +0300

    upload avatar rename bug

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,8 +1,13 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import gravatar from 'gravatar';
+import fs from 'fs/promises';
+import path from 'path';
+import Jimp from 'jimp';
 import authServices from '../services/authServices.js';
 import ctrlWrapper from '../decorators/ctrlWrapper.js';
 import HttpError from '../helpers/HttpError.js';
+import User from '../models/User.js';
 
 const { JWT_SECRET } = process.env;
 
@@ -11,10 +16,12 @@ const singup = async (req, res) => {
   const user = await authServices.findUser({ email });
   if (user) throw HttpError(409, 'Email in use');
   const hashPassword = await bcrypt.hash(password, 10);
-  const newUser = await authServices.singup({ ...req.body, password: hashPassword });
+  const avatarURL = gravatar.url(email);
+  const newUser = await authServices.singup({ ...req.body, password: hashPassword, avatarURL });
   res.status(201).json({
     user: {
       email: newUser.email,
+      avatarURL: newUser.avatarURL,
       subscription: newUser.subscription,
     },
   });
@@ -34,14 +41,15 @@ const singin = async (req, res) => {
     token,
     user: {
       email: user.email,
+      avatarURL: user.avatarURL,
       subscription: user.subscription,
     },
   });
 };
 
 const getCurrent = async (req, res) => {
-  const { email, subscription } = req.user;
-  res.json({ email, subscription });
+  const { email, avatarURL, subscription } = req.user;
+  res.json({ email, avatarURL, subscription });
 };
 
 const logout = async (req, res) => {
@@ -50,9 +58,33 @@ const logout = async (req, res) => {
   res.status(204).send();
 };
 
+const avatarsDir = path.resolve('public', 'avatars');
+
+const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+  const { path: tmpUpload, originalname } = req.file;
+  Jimp.read(tmpUpload)
+    .then(avatar => {
+      return avatar
+        .resize(250, 250) // resize
+        .write(originalname); // save
+    })
+    .catch(error => {
+      throw HttpError(400, error.message);
+    });
+  const resultUpload = path.join(avatarsDir, originalname);
+  await fs.rename(tmpUpload, resultUpload);
+  const avatarURL = path.join('avatars', originalname);
+  await User.findByIdAndUpdate(_id, { avatarURL });
+  res.json({
+    avatarURL,
+  });
+};
+
 export default {
   singup: ctrlWrapper(singup),
   singin: ctrlWrapper(singin),
   getCurrent: ctrlWrapper(getCurrent),
   logout: ctrlWrapper(logout),
+  updateAvatar: ctrlWrapper(updateAvatar),
 };
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -3,11 +3,13 @@ import authControllers from '../controllers/authControllers.js';
 import { userSingupSinginSchema } from '../schemas/usersSchemas.js';
 import validateBody from '../decorators/validateBody.js';
 import authenticate from '../middlewares/authenticate.js';
+import upload from '../middlewares/upload.js';
 
 const authRouter = express.Router();
 
 authRouter.post('/register', validateBody(userSingupSinginSchema), authControllers.singup);
 authRouter.post('/login', validateBody(userSingupSinginSchema), authControllers.singin);
+authRouter.patch('/avatars', authenticate, upload.single('avatar'), authControllers.updateAvatar);
 authRouter.get('/current', authenticate, authControllers.getCurrent);
 authRouter.post('/logout', authenticate, authControllers.logout);
 
